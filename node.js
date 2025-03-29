3a0c39f807f0ac4aa631f7e6e17abd57f7e96aa0
Bipanshu21 <bipanshukumar21@gmail.com>
2025-03-29 18:01:15 +0530

    Project version 0.1

--- node.js
+++ node.js
@@ -7,6 +7,36 @@ const rl = readline.createInterface({
 
 const todo = []
 
+const showMenu = () =>{
+     console.log('\n 1: Add a Task');
+     console.log('2: View Tasks');
+     console.log('3: Exit');
+     rl.question("Choose an option : ", (option)=>{
+           let data = parseInt(option);
+           if(data === 1){
+               rl.question("Enter a task : ", (task) => {
+                    todo.push(task);
+                    console.log("Added task" ,task);
+                    showMenu();
+               });
+           }
+           else if(data === 2){
+                todo.forEach((task, index) => {
+                     console.log(`${index+1} : ${task}`);
+                })
+                showMenu();
+           }else if(data === 3){
+                console.log("Good bye brother");
+                rl.close();
+           }else{
+                console.log("Invalid option, please try again!!");
+                showMenu();
+           }
+     })
+}
+
+showMenu();
+
 
 
 
